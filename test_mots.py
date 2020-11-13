4d3a8282baadf52f2079473db40917864fbbe5e3
Marilou Scravaglieri <marilou@Atlantis.home>
2020-11-13 17:59:51 +0100

    Ajout des tests et des doc de référence

---
+++ test_mots.py
@@ -0,0 +1,10 @@
+#alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p"]
+#import numpy as np
+
+#def genere():
+#    mot = ""
+#    taille_mot = np.random.randint(2, 8)
+#    for i in range(taille_mot):
+#        proba = np.random.randint(0, len(alphabet))
+#        mot += alphabet[proba]
+#    return mot
