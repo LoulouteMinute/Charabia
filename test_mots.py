cdb1e84a40ffbe7b1e0adeb3eda7ed4b80db7f9f
Marilou Scravaglieri <marilou@Atlantis.home>
2020-11-25 15:52:51 +0100

    idee implementation de la generation du charabia

--- test_mots.py
+++ test_mots.py
@@ -1,5 +1,5 @@
 #alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p"]
-#import numpy as np
+import numpy as np
 
 #def genere():
 #    mot = ""
@@ -8,3 +8,14 @@
 #        proba = np.random.randint(0, len(alphabet))
 #        mot += alphabet[proba]
 #    return mot
+
+
+def genere_charabia(mat_enchainement, mat_size, alphabet, tailles):
+    size = np.random.choice(tailles, size = None, replace = False, p = mat_size)
+    ch = "\n"
+    for i in range(size):
+        tab = mat_enchainement[alphabet.index(ch[i])]
+        new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
+        ch += new_letter
+    ch = ch[1:]
+    return ch
