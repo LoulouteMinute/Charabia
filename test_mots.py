260a65a883b5551f36efafe4be1914bc785b194c
Marilou Scravaglieri <marilou@Atlantis.home>
2020-12-03 23:11:03 +0100

    Nouvelle version de l'algo avec methode dictionnaire

--- test_mots.py
+++ test_mots.py
@@ -1,4 +1,4 @@
-#alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p"]A
+#alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p"]
 #import numpy as np
 
 #def genere():
@@ -11,40 +11,55 @@
 
 import numpy as np
 import time
+import pdb
 
 def mat_proba(doc_ref, alphabet):
-    l = doc_ref.read()
-    mat = np.zeros(shape=(len(alphabet),len(alphabet)))
-    for i in range(len(alphabet)):
-        for j in range(len(l)):
-            if l[j] == alphabet[i] and j < len(l)-1:
-                suiv = l[j+1]
-                mat[i, alphabet.index(suiv)] += 1
-            else:
-                mat[i, len(alphabet) - 1] += 1
-    somme_tot = np.sum(mat)
+    mots = doc_ref.readlines()
+    ench = []
+    dico = {}
+    somme_ench = [0]*(len(alphabet))
+    mat = np.zeros(shape = (len(alphabet),len(alphabet)))
+    for i in alphabet:
+        for j in alphabet:
+            clef = i+j
+            dico[clef] = 0
+    for mot in mots :
+        for i in range(len(mot)-1):
+            ench.append(mot[i]+mot[i+1])
+    ench.sort()
+    for clef in dico:
+        i = 0
+        if ench != [] and ench[i] == clef:
+            while ench != [] and ench[i] == clef:
+                dico[clef] += 1
+                ench.remove(clef)
     for i in range(len(alphabet)):
         for j in range(len(alphabet)):
-            if somme_tot != 0:
-                mat[i, j] = (mat[i, j])/somme_tot
+            mat[i, j] += dico[alphabet[i]+alphabet[j]]
+            somme_ench[i] += mat[i, j]
+#            if j == len(alphabet)-1 and somme_ench[i] != 0:
+#                mat[i, j] = (mat[i, j])/somme_ench[i]
+    for i in range(len(alphabet)):
+        for j in range(len(alphabet)):
+            if somme_ench[i] != 0:
+                mat[i, j] = (mat[i, j])/somme_ench[i]
     return mat
 
 def main():
     lettres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'à', 'â', 'é', 'è', 'ê', 'ë', 'ç', 'ï', 'î', 'ô', 'ù', 'û', 'ü', 'æ', 'œ', '\n']
+    lettres.sort()
     start_time = time.time()
     with open('/Users/marilou/Documents/dev/ProjetAlgoL2/liste_mots.txt', 'r') as ref:
         matrice = mat_proba(ref, lettres)
-    print(matrice)
     print(time.time() - start_time, "seconds")
-    for i in range(len(lettres)):
-        for j in range(len(lettres)):
-            print(matrice[i, j])
-    sommes = np.sum(matrice, axis = 0)
-    total = 0
+
+    somme = [0]*42
+    total = np.sum(matrice)
     print("valeurs des sommes")
-    for i in sommes:
-        print(i)
-        total += i
+    for i in range(42):
+        for j in range(42):
+            somme[i] += matrice[i, j]
+    print(somme)
     print("le total vaut:", total)
 
 
