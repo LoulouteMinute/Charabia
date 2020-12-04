5687dcb7accc290bde0e1380c052a98c5ba9c9f1
Marilou Scravaglieri <marilou@Atlantis.home>
2020-12-04 22:52:45 +0100

    comparaison methode tableau vs methode dico

--- test_mots.py
+++ test_mots.py
@@ -13,7 +13,24 @@ import numpy as np
 import time
 import pdb
 
-def mat_proba(doc_ref, alphabet):
+def mat_proba_1(doc_ref, alphabet):
+    l = doc_ref.read()
+    mat = np.zeros(shape=(len(alphabet),len(alphabet)))
+    somme_tot = [0]*len(alphabet)
+    for i in range(len(alphabet)):
+        for j in range(len(l)):
+            if l[j] == alphabet[i] and j < len(l)-1:
+                suiv = l[j+1]
+                mat[i, alphabet.index(suiv)] += 1
+    for i in range(len(alphabet)):
+        for j in range(len(alphabet)):
+            somme_tot[i] += mat[i, j]
+        for j in range(len(alphabet)):
+            if somme_tot[i] != 0:
+                mat[i, j] = (mat[i, j])/somme_tot[i]
+    return mat
+
+def mat_proba_2(doc_ref, alphabet):
     mots = doc_ref.readlines()
     ench = []
     dico = {}
@@ -37,8 +54,6 @@ def mat_proba(doc_ref, alphabet):
         for j in range(len(alphabet)):
             mat[i, j] += dico[alphabet[i]+alphabet[j]]
             somme_ench[i] += mat[i, j]
-#            if j == len(alphabet)-1 and somme_ench[i] != 0:
-#                mat[i, j] = (mat[i, j])/somme_ench[i]
     for i in range(len(alphabet)):
         for j in range(len(alphabet)):
             if somme_ench[i] != 0:
@@ -48,19 +63,31 @@ def mat_proba(doc_ref, alphabet):
 def main():
     lettres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'à', 'â', 'é', 'è', 'ê', 'ë', 'ç', 'ï', 'î', 'ô', 'ù', 'û', 'ü', 'æ', 'œ', '\n']
     lettres.sort()
-    start_time = time.time()
+    print(lettres, len(lettres))
+    start_time_a = time.time()
     with open('/Users/marilou/Documents/dev/ProjetAlgoL2/liste_mots.txt', 'r') as ref:
-        matrice = mat_proba(ref, lettres)
-    print(time.time() - start_time, "seconds")
+        a = mat_proba_1(ref, lettres)
+    print('Temps pour matrice avec tableaux:', time.time() - start_time_a, 'secondes')
+    start_time_b = time.time()
+    with open('/Users/marilou/Documents/dev/ProjetAlgoL2/liste_mots.txt', 'r') as ref:
+        b = mat_proba_2(ref, lettres)
+    print('Temps pour matrice avec dico:', time.time() - start_time_b, "secondes")
 
-    somme = [0]*42
-    total = np.sum(matrice)
-    print("valeurs des sommes")
-    for i in range(42):
+#    print(np.array_equal(a, b))
+    for i in range(3):
         for j in range(42):
-            somme[i] += matrice[i, j]
-    print(somme)
-    print("le total vaut:", total)
+            print(lettres[i], lettres[j], a[i, j], b[i, j])
+
+#    somme = [0]*42
+#    total = 0
+#    print("valeurs des sommes")
+#    for i in range(42):
+#        for j in range(42):
+#            somme[i] += b[i, j]
+#        total += somme[i]
+#    print(somme)
+#    print("le total vaut:", total)
+
 
 
 main()
