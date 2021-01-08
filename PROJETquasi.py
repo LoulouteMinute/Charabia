fa636e5d04d5c3ad6dbd869c19ea853db9895dc8
Marilou Scravaglieri <marilou@Atlantis.home>
2021-01-08 22:21:13 +0100

    Corrections diverses

--- PROJETquasi.py
+++ PROJETquasi.py
@@ -4,26 +4,24 @@ import numpy as np
 import string
 import re
 import os
-os.chdir('C:\\Users\\chhar_000\\Documents\\etudes\\L2 Cpes\\algorithmiqueinfo\\PROJET S3')
+os.chdir('/Users/marilou/Documents/dev/ProjetAlgoL2')
 
 
 #Matrice statistiques tailles des mots
 def longueur(doc_ref):
-    stats = 27*[0]
-    docstr = doc_ref.read()
-    Lmots = re.split(r'(\W+)', docstr)
+    stats = 63*[0]
+    Lmots = re.split(r'(\W+)', doc_ref)
     for mot in Lmots:
         stats[len(mot)+1] += 1/(len(Lmots))
     return stats
 
 #Matrice de probabilités
 def mat_probas(doc_ref, alphabet):
-    l = doc_ref.read()
     mat = np.zeros(shape=(len(alphabet),len(alphabet)))
     somme_tot = [0]*len(alphabet)
-    for i in range(len(l)-1):
-        lettre = l[i]
-        suiv = l[i+1]
+    for i in range(len(doc_ref)-1):
+        lettre = doc_ref[i]
+        suiv = doc_ref[i+1]
         if lettre in alphabet and suiv in alphabet:
             mat[alphabet.index(lettre), alphabet.index(suiv)] += 1
     for i in range(len(alphabet)):
@@ -36,11 +34,10 @@ def mat_probas(doc_ref, alphabet):
 
 #Matrice de probilité spécifique à la fin des mots (trois dernières lettres)
 def mat_proba_fin(doc_ref, alphabet):
-    l = doc_ref.read()
-    mots = l.split(' ')
+    mots = doc_ref.split(' ')
     mat_fin = np.zeros(shape=(len(alphabet),len(alphabet)))
     somme_tot_fin = [0]*len(alphabet)
-    for ch in l:
+    for ch in doc_ref:
         if len(ch) > 5:
             for i in range(-4, -2, 1):
                 lettre = ch[i]
@@ -58,46 +55,18 @@ def mat_proba_fin(doc_ref, alphabet):
 #traitement du texte de reference
 def traitement(reference):
     texte = reference.read()
+    tab = texte.split(' ')
     ponctuation = list(range(33, 41)) + list(range(91, 61)) + list(range(123, 127))
     majuscules = list(range(65, 91))
-    for caractere in texte:
-        if ord(caractere) in ponctuation:
-            caractere = ''
-        if ord(caractere) in majuscules:
-            caractere.lower()
+    for i in range(len(tab)):
+        if len(tab[i]) == 1 and ord(tab[i]) in ponctuation:
+            tab.pop(i)
+        if len(tab[i]) > 1  and ord(tab[i][0]) in majuscules:
+            tab[i][0].lower()
+    texte = ''.join(tab)
+    texte = texte.replace('   ', ' ')
+    texte = texte.replace('  ', ' ')
     return texte
-
-#Outils généraux:
-lettres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","é","è","ê","ë","à","â","î","ï","ô","ù","û","ü","æ","œ","ç","\n"]
-lettres.sort()
-tailles = list(range(1,28))
-
-#traitement texte de reference pour les matrices
-ref = open('ref_francais.txt', 'r', encoding = 'utf8')
-texte = traitement(ref)
-#Creation des matrices de probas:
-start_time = time.time()
-#matrice d'enchainement
-ref = open('liste2.txt','r',encoding='utf8')
-prob = mat_probas(ref, lettres)
-ref.close()
-#matrice de d'enchainement spécifique aux fins de mots
-ref = open('liste2.txt','r',encoding='utf8')
-prob = mat_probas(ref, lettres)[0]
-somme_tot = mat_probas(ref, lettres)[1]
-prob_fin = mat_proba_fin(ref, lettres)[0]
-somme_tot_fin = mat_proba_fin(ref, lettres)[1]
-for i in range(len(lettres)):
-    if somme_tot_fin[i] == 0:
-        prob_fin[i] = prob[i]
-#matrice de tailles des mots:
-ref = open('liste2.txt','r',encoding='utf8')
-Lstats = longueur(ref)
-ref.close()
-
-print(time.time() - start_time)
-print(prob,'\n',Lstats)
-
 
 #Fonction pour la generation d'un mot charabia de taille donnée:
 def genere_charabia1(mat_enchainement, mat_enchainement_fin, alphabet, taille):
@@ -152,7 +121,7 @@ def genere_charabia2(mat_enchainement, mat_enchainement_fin, mat_size, alphabet,
         for i in range(size):
             tab = mat_enchainement[alphabet.index(ch[i])]
             new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
-            while new_letter == '\n' or (new_letter==ch[-2] and new_letter==ch[-1]):
+            while new_letter == '\n' or (new_letter == ch[-2] and new_letter == ch[-1]):
                 new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
             ch += new_letter
         for i in range(3):
@@ -193,15 +162,17 @@ def genere_charabia3(premiere_lettre, mat_enchainement, mat_enchainement_fin, ma
                     new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
                 ch += new_letter
         else:
+            tab = mat_enchainement_fin[alphabet.index(ch[-1])]
             for i in range(size-1):
-                tab = mat_enchainement_fin[alphabet.index(ch[-1])]
                 new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
-                while new_letter == '\n' or (new_letter == ch[-2] and new_letter == ch[-1]):
+                while new_letter == '\n':
                     new_letter = np.random.choice(alphabet, size = None, replace = False, p = tab)
                 ch += new_letter
+            while ch[0] ==  ch[1] or ch[1] == ch[2]:
+                ch[1] = np.random.choice(alphabet, size = None, replace = False, p = tab)
         return ch
     elif premiere_lettre in string.ascii_uppercase:
-        ch=premiere_lettre.lower()
+        ch = premiere_lettre.lower()
         if size > 3:
             for i in range(size-4):
                 tab = mat_enchainement[alphabet.index(ch[i])]
@@ -236,6 +207,37 @@ def genere_charabia4(dernieres_lettres, mat_enchainement,mat_size, alphabet, tai
 
 #Remplacer les mots d'un texte par du charabia:
 def main():
+    #Outils généraux:
+    lettres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","é","è","ê","ë","à","â","î","ï","ô","ù","û","ü","æ","œ","ç","\n", " "]
+    lettres.sort()
+    tailles = list(range(1,64))
+
+    #traitement texte de reference pour les matrices
+    texte_ref = open('ref_francais.txt', 'r', encoding = 'utf8')
+    ref = traitement(texte_ref)
+    texte_ref.close()
+
+    #Creation des matrices de probas:
+    start_time = time.time()
+
+    #matrice d'enchainement
+    prob = mat_probas(ref, lettres)
+
+    #matrice de d'enchainement spécifique aux fins de mots
+    prob = mat_probas(ref, lettres)[0]
+    somme_tot = mat_probas(ref, lettres)[1]
+    prob_fin = mat_proba_fin(ref, lettres)[0]
+    somme_tot_fin = mat_proba_fin(ref, lettres)[1]
+    for i in range(len(lettres)):
+        if somme_tot_fin[i] == 0:
+            prob_fin[i] = prob[i]
+
+    #matrice de tailles des mots:
+    Lstats = longueur(ref)
+
+    print(time.time() - start_time)
+    print(prob,'\n',Lstats)
+
     print('\n\nBienvenue au générateur de charabia Chamama \n\nQuel texte voulez-vous charabier?')
     nom_fichier = str(input('introduire nom_du_fichier.txt:\n'))
     doc = open(nom_fichier, 'r', encoding='utf8')
@@ -256,11 +258,11 @@ def main():
         else:
             a_remplacer = Lmots[i]
             if mode == 'T':
-                Lmots[i] = genere_charabia1(prob,lettres,len(a_remplacer))
+                Lmots[i] = genere_charabia1(prob, prob_fin,lettres,len(a_remplacer))
             elif mode == 'A':
-                Lmots[i] = genere_charabia2(prob,Lstats,lettres,tailles)
+                Lmots[i] = genere_charabia2(prob, prob_fin, Lstats, lettres, tailles)
             elif mode == 'P':
-                Lmots[i] = genere_charabia3(a_remplacer[0],prob,Lstats,lettres,tailles)
+                Lmots[i] = genere_charabia3(a_remplacer[0], prob, prob_fin, Lstats, lettres, tailles)
             elif mode == 'S':
                 long_suffixe = int(input('Entrez la longueur du suffixe: '))
                 Lmots[i] = genere_charabia4(a_remplacer[-long_suffixe],prob,Lstats,lettres,tailles)
